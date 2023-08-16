c5966fd24b7005f2344113020cb2e7b9fe54eada
C4buz <140349703+C4buz@users.noreply.github.com>
2023-08-17 10:22:01 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,9 +1,9 @@
 const SLICE_COUNT = 12;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_GIF(1000));
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(true);
+  pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
 }
@@ -14,42 +14,41 @@ function setup_layers(pScope){
 
   var layer1 = new PLayer(faces);
   layer1.mode( SWIRL(2) );
-  layer1.set_boundary( 100, 600 );
-
-  var layer2 = new Player(outer);
+  layer1.set_boundary(100, 600);
+  
+  var layer2 = new PLayer(outer,color(116, 150, 125) );
   layer2.mode( RING );
-  layer2.set_boundary(0,400);
+  layer2.set_boundary(650,1000);
 
-
+  // var layer3 = new PLayer(dots);
+  // layer3.mode( SWIRL(5) );
+  // layer3.set_boundary(100, 600);
+  
 }
 
 function faces (x,y,animation,pScope){
 scale (animation.frame*2);
 
 strokeWeight (0)
-fill (94, 130, 94)
+fill (116, 150, 125)
 ellipse (0,50,80,80) //Outer Circle
 
-fill (225,255,255)
-ellipse (0,50,52,52) // Middle White Outline
+}
 
-fill (115, 160, 199)
-ellipse (0,50, 50,50) // Middle Circle
+function outer (x, y, animation,pScope){
+fill (0)
+ellipse (-10,-830-animation.wave()*50,50,200)
 
-fill (177, 192, 204) // Inner Circle
-ellipse (0,50,20,20)
 
 }
 
-function outer (x, y, animation, pScope){
-let angleOffset = (360 / SLICE_COUNT) / 2
-let backgroundArcStart = 270 - angleOffset;
-let backgroundArcEnd = 270 + angleOffset;
+function dots (x,y,animation,pScope){
+scale (animation.frame*2);
 
-fill (115, 160, 199)
-arc (x,y,800,800,backgroundArcStart,backgroundArcEnd);
-
-fill (255)
-rect (-10,-300-animation.wave()*50,20,20)
+strokeweight (0)
+fill (255,255,255)
+ellipse (0,50,10,10)
 
 }
+
+//use recordings to learn spinning objects and colour changes
