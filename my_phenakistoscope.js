30f78585ebef42c23a3b1b09be6dae679fa3bed8
C4buz <140349703+C4buz@users.noreply.github.com>
2023-08-29 19:58:30 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,4 +1,4 @@
-const SLICE_COUNT = 12;
+const SLICE_COUNT = 15;
 
 function setup_pScope(pScope){
   pScope.output_mode(ANIMATED_DISK);
@@ -6,6 +6,7 @@ function setup_pScope(pScope){
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+<<<<<<< Updated upstream
   pScope.load_image_sequence("jelly1.png","png",1)
   pScope.load_image_sequence("jelly2.png","png",2)
   pScope.load_image_sequence("jelly3.png","png",3)
@@ -13,6 +14,9 @@ function setup_pScope(pScope){
   pScope.load_image_sequence("jelly5.png","png",5)
   pScope.load_image_sequence("jelly6.png","png",6)
   pScope.load_image_sequence("jelly7.png","png",7)
+=======
+  pScope.load_image("Heart","png");
+>>>>>>> Stashed changes
 }
 
 function setup_layers(pScope){
@@ -23,10 +27,20 @@ function setup_layers(pScope){
   layer1.mode( SWIRL(2) );
   layer1.set_boundary(100, 600);
   
+<<<<<<< Updated upstream
   var layer2 = new PLayer(outer,color(172, 204, 232) );
   layer2.mode( RING );
   layer2.set_boundary(650,1000);
 
+=======
+  var layer2 = new PLayer(outer,color(189, 121, 102) );
+  layer2.mode( RING );
+  layer2.set_boundary(650,1000);
+
+ var layer3 = new PLayer (hearts);
+ layer3.mode (SWIRL(2));
+ layer3.set_boundary(100,1000);
+>>>>>>> Stashed changes
   
 }
 
@@ -39,6 +53,12 @@ pScope.draw_image_from_sequence("jelly5.png","png", x, y, animation.frame);
 pScope.draw_image_from_sequence("jelly6.png","png", x, y, animation.frame);
 pScope.draw_image_from_sequence("jelly7.png","png", x, y, animation.frame);
 
+<<<<<<< Updated upstream
+=======
+strokeWeight (0)
+fill (189, 121, 102)
+ellipse (0,50,80,80) //Outer Circle
+>>>>>>> Stashed changes
 
 }
 
@@ -47,6 +67,17 @@ function outer (x, y, animation,pScope){
 
 
 }
+
+<<<<<<< Updated upstream
+
+=======
+function hearts (x,y,animation,pScope){
+scale (0.2)
+  pScope.draw_image("Heart",x,y);
+
+
+}
+>>>>>>> Stashed changes
 
 
 
