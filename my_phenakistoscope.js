95c9e8dfc7d4de9532dad1d5547dd08d4a80ba02
C4buz <140349703+C4buz@users.noreply.github.com>
2023-09-04 19:36:53 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,12 +1,14 @@
 const SLICE_COUNT = 12;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_FRAME);
+  pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image_sequence("jelly","png",7)
+  pScope.load_image("fish_1" , "png");
+  pScope.load_image("Background" , "png");
   
 }
 
@@ -14,16 +16,18 @@ function setup_layers(pScope){
 
   new PLayer(null, 52, 75, 138);  //lets us draw the whole circle background, ignoring the boundaries
   
-  var Rim = new PLayer(outer,color(143, 160, 207) );
-  Rim.mode( RING );
-  Rim.set_boundary(950,1000);
+  // var background = new PLayer (Background)
+    
+  // var Rim = new PLayer(outer,color(143, 160, 207) );
+  // Rim.mode( RING );
+  // Rim.set_boundary(950,1000);
 
   // var whitefish = new PLayer (white_fish)
   // whitefish.mode(RING)
   // whitefish.set_boundary (0,1000)
 
   var JellyFish = new PLayer(Jelly);
-  JellyFish.mode( SWIRL(2) );
+  JellyFish.mode( RING );
   JellyFish.set_boundary(0, 1000);
 
   var layer4 = new PLayer (bubbles)
@@ -46,7 +50,13 @@ function setup_layers(pScope){
   
 }
 
+function Background (x,y,animation,pScope){
+  push()
+  scale (0.4)
+  pScope.draw_image("Background",0,0);
+  pop()
 
+}
 function outer (x, y, animation,pScope){
 
 
@@ -54,7 +64,7 @@ function outer (x, y, animation,pScope){
 }
 function Jelly (x,y,animation,pScope){
   scale (0.9)
-  pScope.draw_image_from_sequence("jelly", 0, 0, animation.frame);
+  pScope.draw_image_from_sequence("jelly", 0, 700, animation.frame);
   
 }
 function middle (x,y,animation, pScope){
@@ -72,11 +82,16 @@ ellipse (0,40-animation.wave()*100,30,30)
 ellipse (80,0-animation.wave()*100,30,30)
 }
 function orange_fish (x,y,animation,pScope){
-  
-  fill (245, 139, 69)
-  ellipse (500,150-animation.wave()*30,20,50)
-  ellipse (450,100-animation.wave()*30,20,50)
-  ellipse (400,200-animation.wave()*30,20,50)
+  push()
+  scale (0.02)
+  pScope.draw_image("fish_1",0-animation.wave()*600,-20000);
+  pScope.draw_image("fish_1",4000-animation.wave()*600,-18000);
+  pScope.draw_image("fish_1",-2000-animation.wave()*600,-15000);
+  pop()
+  // fill (245, 139, 69)
+  // ellipse (500,150-animation.wave()*30,20,50)
+  // ellipse (450,100-animation.wave()*30,20,50)
+  // ellipse (400,200-animation.wave()*30,20,50)
 
 }
 function white_fish (x,y,animation,pScope){
