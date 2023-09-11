8ebf4895f78ed6367f7cbdee38f9bff13075e512
C4buz <140349703+C4buz@users.noreply.github.com>
2023-09-11 22:20:51 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -11,23 +11,20 @@ function setup_pScope(pScope){
   pScope.load_image_sequence("grass","png",2)
   pScope.load_image_sequence("fish","png",2)
 
-  // pScope.load_image("fish_1" , "png");
   pScope.load_image("coral", "png");
   pScope.load_image("coralbluegreen", "png");
   pScope.load_image("coralpinksmall", "png");
   pScope.load_image("coralblue", "png");
 
- 
 
  let coral_base_colour = "#143752" // this is stopping it from working?
 }
 
 function setup_layers(pScope){
-   new PLayer(null, 14, 99, 125);  //lets us draw the whole circle background, ignoring the boundaries
-  
-  var background = new PLayer (Background)
  
-
+  new PLayer(null, 39, 99, 145);  //lets us draw the whole circle background, ignoring the boundaries
+  
+   
   var orangefish = new PLayer (orange_fish);
   orangefish.mode(RING)
   orangefish.set_boundary (0,1000)
@@ -36,15 +33,7 @@ function setup_layers(pScope){
   JellyFish.mode( RING );
   JellyFish.set_boundary(0, 1000);
 
-  var coral_centre = new PLayer (middle);
-  coral_centre.mode
-  coral_centre.set_boundary (0,200);
-
-  // var edgeC = new PLayer (edge);
-  // edgeC.mode
-  // edgeC.set_boundary (940,1000);
-
-  var plantscentre = new PLayer (coral_plants);
+   var plantscentre = new PLayer (coral_plants);
   plantscentre.mode (RING)
   plantscentre.set_boundary (0,1000);
   
@@ -52,18 +41,14 @@ function setup_layers(pScope){
   layer4.mode(SWIRL(1));
   layer4.set_boundary (200,500);
 
-}
+  var coral_centre = new PLayer (middle);
+  coral_centre.mode
+  coral_centre.set_boundary (0,175);
 
-function Background (){
-
-//  pScope.fill_background(animating_colour);
-
-//  let StartColour = color(14, 99, 125);
-//  let EndColour = color (118, 186, 207);
-
-//  let animating_colour = lerpColor(StartColour,EndColour,animation.frame)
+  
 
 }
+
 function outer (x, y, animation,pScope){
 
 
@@ -97,12 +82,12 @@ pop()
 }
 function orange_fish (x,y,animation,pScope){
   
-  push() // Original
-  scale (0.02)
+  push() 
+  scale (0.017)
 
-  pScope.draw_image_from_sequence("fish",8000-animation.wave()*600,-19000, animation.frame);
-  pScope.draw_image_from_sequence("fish",4000-animation.wave()*600,-17000, animation.frame);
-  pScope.draw_image_from_sequence("fish",1000-animation.wave()*600,-19000, animation.frame);
+  pScope.draw_image_from_sequence("fish",4000-animation.wave()*600,-20000, animation.frame);
+  pScope.draw_image_from_sequence("fish",0-animation.wave()*600,-17000, animation.frame);
+  pScope.draw_image_from_sequence("fish",-2000-animation.wave()*600,-19000, animation.frame);
 
   pop()
  
@@ -110,32 +95,22 @@ function orange_fish (x,y,animation,pScope){
 
 }
 
-function edge (x,y,animation,pScope) {
-
-  pScope.fill_background (20, 55, 82)
-}
-
 function coral_plants (x,y,animation,pScope){
   
   push()
   scale (0.10)
-  pScope.draw_image_from_sequence("grass", 0-animation.wave()*10, 2400, animation.frame);
+  pScope.draw_image_from_sequence("grass", 0-animation.wave()*10, 2200, animation.frame);
   pop ()
   
   push()
-  scale (0.14)
-  pScope.draw_image("coralblue",0-animation.wave()*10,1300);
+  scale (0.11)
+  pScope.draw_image("coralblue",150-animation.wave()*10,1750);
   pop ()
   
   // push()
   // scale (0.13)
   // pScope.draw_image("coralpinksmall",0-animation.wave()*10,1200);
   // pop ()
-
-
-
-
- 
 
 }
 
