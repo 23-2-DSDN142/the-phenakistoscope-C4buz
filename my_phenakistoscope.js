76fd411b2de3ad7acf8beae169769b90ff416598
C4buz <140349703+C4buz@users.noreply.github.com>
2023-09-11 22:56:05 +1200

    Update my_phenakistoscope.js

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -15,16 +15,12 @@ function setup_pScope(pScope){
   pScope.load_image("coralbluegreen", "png");
   pScope.load_image("coralpinksmall", "png");
   pScope.load_image("coralblue", "png");
-
-
- let coral_base_colour = "#143752" // this is stopping it from working?
 }
 
 function setup_layers(pScope){
  
   new PLayer(null, 39, 99, 145);  //lets us draw the whole circle background, ignoring the boundaries
   
-   
   var orangefish = new PLayer (orange_fish);
   orangefish.mode(RING)
   orangefish.set_boundary (0,1000)
@@ -38,15 +34,12 @@ function setup_layers(pScope){
   plantscentre.set_boundary (0,1000);
   
   var layer4 = new PLayer (bubbles)
-  layer4.mode(SWIRL(1));
+  layer4.mode(SWIRL (1));
   layer4.set_boundary (200,500);
 
   var coral_centre = new PLayer (middle);
   coral_centre.mode
   coral_centre.set_boundary (0,175);
-
-  
-
 }
 
 function outer (x, y, animation,pScope){
@@ -61,33 +54,40 @@ function Jelly (x,y,animation,pScope){
 }
 function bubbles (x,y,animation,pScope){
 
-let end_colour = color(255,255,255)
+let backgroundcolour = ("39, 99, 145")
+
+let end_colour = color(backgroundcolour)
 let start_colour = color(52,75,138)
 let animating_colour = lerpColor(start_colour,end_colour,animation.frame)
+
 stroke (animating_colour)
 strokeWeight (2)
 noFill()
+
+let bubblesize = 30
+let bubbleshine = 8
+
 push()
 scale (0.7)
 
-ellipse (-80,0-animation.wave()*100,30,30) // Outer Ring
-ellipse (0,40-animation.wave()*100,30,30)
-ellipse (80,0-animation.wave()*100,30,30)
-
-ellipse (-76,-2-animation.wave()*100,8,8) // Inner Ring
-ellipse (4,38-animation.wave()*100,8,8)
-ellipse (84,-2-animation.wave()*100,8,8)
-
+ellipse (-80,0-animation.wave()*100,bubblesize,bubblesize) // Outer Ring
+ellipse (0,40-animation.wave()*100,bubblesize,bubblesize)
+ellipse (80,0-animation.wave()*100,bubblesize,bubblesize)
+  
+ellipse (-76,-2-animation.wave()*100,bubbleshine,bubbleshine) // Inner Ring
+ellipse (4,38-animation.wave()*100,bubbleshine,bubbleshine)
+ellipse (84,-2-animation.wave()*100,bubbleshine,bubbleshine)
 pop()
+
 }
 function orange_fish (x,y,animation,pScope){
   
   push() 
   scale (0.017)
 
-  pScope.draw_image_from_sequence("fish",4000-animation.wave()*600,-20000, animation.frame);
-  pScope.draw_image_from_sequence("fish",0-animation.wave()*600,-17000, animation.frame);
-  pScope.draw_image_from_sequence("fish",-2000-animation.wave()*600,-19000, animation.frame);
+  pScope.draw_image_from_sequence("fish",6000-animation.wave()*600,-20000, animation.frame);
+  pScope.draw_image_from_sequence("fish",0-animation.wave()*600,-20000, animation.frame);
+  pScope.draw_image_from_sequence("fish",-5000-animation.wave()*600,-17000, animation.frame);
 
   pop()
  
@@ -104,15 +104,10 @@ function coral_plants (x,y,animation,pScope){
   
   push()
   scale (0.11)
-  pScope.draw_image("coralblue",150-animation.wave()*10,1750);
+  pScope.draw_image("coralblue",150-animation.wave()*10,1800);
   pop ()
   
-  // push()
-  // scale (0.13)
-  // pScope.draw_image("coralpinksmall",0-animation.wave()*10,1200);
-  // pop ()
-
-}
+ }
 
 function middle (x,y,animation, pScope){
  
